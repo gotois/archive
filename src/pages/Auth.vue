afaf1e2dfce9ecf019883e8d564cb5307104e145
Deni Baskovsky <qertis@users.noreply.github.com>
2022-08-15 12:18:29 +0300

    Disabled OTP for wrong passcode

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,34 +1,52 @@
 <template>
-  <div class="fullscreen bg-white flex flex-center">
+  <div class="fullscreen bg-white flex flex-center column">
+    <p class="text-caption text-center">Введите ключ</p>
     <v-otp-input
+      ref="otpInput"
       input-classes="otp-input"
       separator="-"
       :num-inputs="4"
+      :is-disabled="otpDisabled"
       :should-auto-focus="true"
       :is-input-num="true"
       :conditional-class="['first', '', '', 'last']"
       :placeholder="['', '', '', '']"
-      @on-complete="handleOnComplete"
+      @on-complete="onHandleComplete"
     />
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
-import {useMeta} from 'quasar'
+import {defineComponent, ref} from 'vue'
+import {QVueGlobals, useMeta, useQuasar} from 'quasar'
 import VOtpInput from 'vue3-otp-input'
 import {Store as VuexStore} from 'vuex'
 import {Router, useRouter} from 'vue-router'
 import {StateInterface, useStore} from '../store'
 
+let $q: QVueGlobals
 let store: VuexStore<StateInterface>
 let router: Router
 
+const timeout = 2000
 const metaData = {
   title: 'Авторизация',
 }
 
-async function handleOnComplete(value: string) {
+const otpDisabled = ref(false)
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const otpInput = ref(null)
+
+function clearOTP() {
+  otpDisabled.value = false
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+  otpInput.value.clearInput()
+  document.querySelectorAll('.otp-input').forEach((element) => {
+    (element as HTMLElement).blur()
+  })
+}
+
+async function onHandleComplete(value: string) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const codeValid = await store.dispatch('Auth/checkCode', value)
   if (codeValid) {
@@ -37,16 +55,28 @@ async function handleOnComplete(value: string) {
     await router.replace(prevPath || {
       name: 'archive',
     })
+  } else {
+    otpDisabled.value = true
+    $q.notify({
+      color: 'negative',
+      message: 'Неверный ключ',
+      timeout: timeout,
+    })
+    setTimeout(() => clearOTP(), timeout)
   }
 }
 
 function main() {
+  $q = useQuasar()
   router = useRouter()
   store = useStore()
   useMeta(metaData)
 
   return {
-    handleOnComplete,
+    otpDisabled,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    otpInput,
+    onHandleComplete,
   }
 }
 
