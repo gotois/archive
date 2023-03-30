ee546a165fcf774e5a78f45418fcb87753af429e
Deni Baskovsky <qertis@users.noreply.github.com>
2023-03-30 22:14:16 +0300

    Дизайн и функционал кнопки "Удалить + Pod" #77

--- src/components/DatabaseRemoveComponent.vue
+++ src/components/DatabaseRemoveComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <q-dialog persistent square transition-show="scale" transition-hide="scale">
-    <q-card class="bg-red text-white" style="width: 300px">
+    <q-card class="bg-red text-white" style="width: 400px">
       <q-card-section>
         <div class="text-h6">{{ $t('settings.clean.submit') }}</div>
       </q-card-section>
@@ -20,18 +20,62 @@
           color="red"
           @click="onClearDatabase"
         />
+        <q-btn
+          v-if="isLoggedIn"
+          flat
+          :label="$t('settings.clean.okAll')"
+          color="red"
+          @click="onClearDatabasePlus"
+        />
       </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
 import { LocalStorage, useQuasar } from 'quasar'
 import { BulkError } from 'dexie'
+import { useStore } from '../store'
 import { db } from '../services/databaseHelper'
+import { removeContractsDataset } from '../services/podHelper'
 
 const emit = defineEmits(['onClear'])
 const $q = useQuasar()
+const store = useStore()
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+const isLoggedIn = computed(() => store.getters['Auth/isLoggedIn'] as boolean)
+
+async function onClearDatabasePlus() {
+  emit('onClear')
+  $q.loading.show()
+  try {
+    await removeContractsDataset()
+    await db.delete()
+    LocalStorage.clear()
+    $q.loading.hide()
+    const timeout = 1500
+    $q.notify({
+      type: 'warning',
+      progress: false,
+      spinner: true,
+      timeout: timeout,
+      message: 'База данных удалена. Дождитесь перезагрузки.',
+    })
+    setTimeout(() => {
+      location.reload()
+    }, timeout)
+  } catch (error) {
+    const msg = (error as BulkError).message
+    console.error(error)
+    $q.loading.hide()
+    $q.notify({
+      message: msg,
+      type: 'negative',
+    })
+  }
+}
 
 async function onClearDatabase() {
   emit('onClear')
