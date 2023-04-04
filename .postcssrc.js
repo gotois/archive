15c7a145726b594b40f92e3cb04a9685620dd450
Deni Baskovsky <qertis@users.noreply.github.com>
2023-04-04 19:13:53 +0300

    New Quasar point #93

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,18 @@
 
 module.exports = {
   plugins: [
-    // to edit target browsers: use "browserslist" field in package.json
-    require('autoprefixer'),
+    // https://github.com/postcss/autoprefixer
+    require('autoprefixer')({
+      overrideBrowserslist: [
+        'last 4 Chrome versions',
+        'last 4 Firefox versions',
+        'last 4 Edge versions',
+        'last 4 Safari versions',
+        'last 4 Android versions',
+        'last 4 ChromeAndroid versions',
+        'last 4 FirefoxAndroid versions',
+        'last 4 iOS versions',
+      ]
+    })
   ]
 }
