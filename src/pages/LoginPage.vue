366eeb8e770536c26296c4959c46564adf5de85c
Deni Baskovsky <qertis@users.noreply.github.com>
2023-05-12 00:03:12 +0300

    Login Page design

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,37 +1,39 @@
 <template>
-  <QPage class="flex column q-ma-md">
-    <template v-if="getOidcIssuer">
-      <p>{{ $t('login.oidcIssuer', { oidcIssuer: getOidcIssuer }) }}</p>
-      <QBtn
-        color="accent"
-        label="Push to Login"
-        :disable="loading"
-        :loading="loading"
-        square
-        glossy
-        push
-        unelevated
-        no-caps
-        @click="onLogin"
-      />
-    </template>
-    <template v-else>
-      <p>{{ $t('login.oidcIssuerInput') }}</p>
-      <OIDCIssuerComponent @on-complete="onOnlineAuthorize" />
-    </template>
+  <QPage class="flex column flex-center q-ma-md">
+    <QCard flat square bordered class="q-pa-md" style="width: 400px">
+      <template v-if="getOidcIssuer">
+        <p>{{ $t('login.oidcIssuer', { oidcIssuer: getOidcIssuer }) }}</p>
+        <QBtn
+          color="accent"
+          :label="$t('login.authentication')"
+          :disable="$q.loading.isActive"
+          :loading="$q.loading.isActive"
+          square
+          glossy
+          push
+          unelevated
+          no-caps
+          class="full-width"
+          @click="onLogin"
+        />
+      </template>
+      <template v-else>
+        <p>{{ $t('login.oidcIssuerInput') }}</p>
+        <OIDCIssuerComponent @on-complete="onOnlineAuthorize" />
+      </template>
+    </QCard>
   </QPage>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
-import { Loading, SessionStorage, QBtn, QPage } from 'quasar'
+import { Loading, SessionStorage, QBtn, QPage, QCard } from 'quasar'
 import usePodStore from 'stores/pod'
 import OIDCIssuerComponent from 'components/OIDCIssuerComponent.vue'
 import solidAuth from '../services/authHelper'
 
 const podStore = usePodStore()
 
-const loading = ref(false)
 const { getOidcIssuer } = storeToRefs(podStore)
 
 async function onLogin() {
@@ -41,7 +43,11 @@ async function onLogin() {
 
 async function tryLogin() {
   try {
-    loading.value = true
+    Loading.show({
+      message: 'Идет авторизация. Пожалуйста, подождите...',
+      boxClass: 'bg-grey-2 text-grey-9',
+      spinnerColor: 'primary',
+    })
     await solidAuth({
       redirectUrl: window.location.origin,
       oidcIssuer: getOidcIssuer.value,
@@ -50,19 +56,13 @@ async function tryLogin() {
   } catch (e) {
     console.error(e)
     SessionStorage.remove('restorePreviousSession')
-    Loading.hide()
   } finally {
-    loading.value = false
+    Loading.hide()
   }
 }
 
 onMounted(async () => {
   if (getOidcIssuer.value) {
-    Loading.show({
-      message: 'Идет авторизация. Пожалуйста, подождите...',
-      boxClass: 'bg-grey-2 text-grey-9',
-      spinnerColor: 'primary',
-    })
     await tryLogin()
   }
 })
