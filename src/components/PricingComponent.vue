9e4ad224834766f0dc681731d80764f68aba6a7a
Deni Baskovsky <qertis@users.noreply.github.com>
2023-09-20 11:34:31 +0300

    Pricing new design

--- src/components/PricingComponent.vue
+++ src/components/PricingComponent.vue
@@ -1,13 +1,18 @@
 <template>
   <QScrollArea class="absolute-full fit">
     <div
-      class="items-start justify-center self-start"
+      class="items-start justify-center self-start items-stretch"
       :class="{
         'row q-pa-md q-gutter-md': $q.platform.is.desktop,
-        'items-stretch q-gutter-xs': $q.platform.is.mobile,
+        'row q-gutter-xs': $q.platform.is.ipad,
       }"
     >
-      <QCard bordered class="col bg-white text-dark col-3">
+      <QCard
+        square
+        :flat="$q.platform.is.mobile"
+        :bordered="$q.platform.is.desktop"
+        class="col bg-white text-dark col-3"
+      >
         <QCardSection class="text-center">
           <div class="text-h5 text-weight-bolder">
             {{ $t('pricing.free.title') }}
@@ -40,19 +45,28 @@
             <QTree
               class="full-width"
               :nodes="freeSupport"
+              :default-expand-all="$q.platform.is.desktop"
+              :dense="$q.platform.is.desktop"
               node-key="label"
               text-color="dark"
             />
             <QTree
               class="full-width"
               :nodes="freeFunctional"
+              :default-expand-all="$q.platform.is.desktop"
+              :dense="$q.platform.is.desktop"
               node-key="label"
               text-color="dark"
             />
           </QList>
         </QCardSection>
       </QCard>
-      <QCard bordered class="bg-primary text-white col-3">
+      <QCard
+        square
+        :flat="$q.platform.is.mobile"
+        :bordered="$q.platform.is.desktop"
+        class="bg-primary text-white col-3"
+      >
         <QCardSection class="text-center">
           <div class="text-h5 text-weight-bolder">
             {{ $t('pricing.premium.title') }}
@@ -78,7 +92,7 @@
             @click="$emit('premium')"
           />
         </QCardSection>
-        <QSeparator inset />
+        <QSeparator inset color="grey-5" />
         <QCardSection>
           <QList
             :dense="$q.platform.is.desktop"
@@ -88,19 +102,28 @@
             <QTree
               class="full-width"
               :nodes="premiumSupport"
+              :default-expand-all="$q.platform.is.desktop"
+              :dense="$q.platform.is.desktop"
               node-key="label"
               text-color="dark"
             />
             <QTree
               class="full-width"
               :nodes="premiumFunctional"
+              :default-expand-all="$q.platform.is.desktop"
+              :dense="$q.platform.is.desktop"
               node-key="label"
               text-color="dark"
             />
           </QList>
         </QCardSection>
       </QCard>
-      <QCard bordered class="bg-dark text-white col-3">
+      <QCard
+        square
+        :flat="$q.platform.is.mobile"
+        :bordered="$q.platform.is.desktop"
+        class="bg-black text-white col-3"
+      >
         <QCardSection class="text-center">
           <div class="text-h5 text-weight-bolder">
             {{ $t('pricing.vip.title') }}
@@ -126,7 +149,7 @@
             @click="$emit('vip')"
           />
         </QCardSection>
-        <QSeparator inset />
+        <QSeparator inset color="grey-8" />
         <QCardSection>
           <QList
             :dense="$q.platform.is.desktop"
@@ -136,12 +159,16 @@
             <QTree
               class="full-width"
               :nodes="vipSupport"
+              :default-expand-all="$q.platform.is.desktop"
+              :dense="$q.platform.is.desktop"
               node-key="label"
               text-color="dark"
             />
             <QTree
               class="full-width"
               :nodes="vipFunctional"
+              :default-expand-all="$q.platform.is.desktop"
+              :dense="$q.platform.is.desktop"
               node-key="label"
               text-color="dark"
             />
@@ -184,6 +211,7 @@ const vipPrice = '$200'
 const freeSupport = ref([
   {
     label: $t('pricing.free.support.title'),
+    icon: 'contact_support',
     children: [
       { label: $t('pricing.free.support.values.1') },
       { label: $t('pricing.free.support.values.2') },
@@ -193,6 +221,7 @@ const freeSupport = ref([
 const premiumSupport = ref([
   {
     label: $t('pricing.premium.support.title'),
+    icon: 'contact_support',
     children: [
       { label: $t('pricing.premium.support.values.1') },
       { label: $t('pricing.premium.support.values.2') },
@@ -202,6 +231,7 @@ const premiumSupport = ref([
 const vipSupport = ref([
   {
     label: $t('pricing.vip.support.title'),
+    icon: 'contact_support',
     children: [
       { label: $t('pricing.vip.support.values.1') },
       { label: $t('pricing.vip.support.values.2') },
@@ -212,6 +242,7 @@ const vipSupport = ref([
 const freeFunctional = ref([
   {
     label: $t('pricing.free.functions.title'),
+    icon: 'shield',
     children: [
       { label: $t('pricing.free.functions.values.1') },
       { label: $t('pricing.free.functions.values.2') },
@@ -221,6 +252,7 @@ const freeFunctional = ref([
 const premiumFunctional = ref([
   {
     label: $t('pricing.premium.functions.title'),
+    icon: 'admin_panel_settings',
     children: [
       { label: $t('pricing.premium.functions.values.1') },
       { label: $t('pricing.premium.functions.values.2') },
@@ -235,6 +267,7 @@ const premiumFunctional = ref([
 const vipFunctional = ref([
   {
     label: $t('pricing.vip.functions.title'),
+    icon: 'security',
     children: [
       { label: $t('pricing.vip.functions.values.1') },
       { label: $t('pricing.vip.functions.values.2') },
