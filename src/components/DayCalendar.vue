634fe8404e7843e72de1295fd6a87b68a518af80
Deni Baskovsky <qertis@users.noreply.github.com>
2024-10-25 21:30:12 +0300

    Day Calendar localization

--- src/components/DayCalendar.vue
+++ src/components/DayCalendar.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="q-pa-lg">
-    <div class="text-grey-8">{{
-      new Intl.DateTimeFormat('ru', { weekday: 'short' }).format(props.date)
-    }}</div>
-    <div class="text-bold">{{ props.date.getDate() }}</div>
+  <div>
+    <div class="text-grey-8">
+      {{
+        new Intl.DateTimeFormat(i18n.locale, { weekday: 'short' }).format(
+          props.date,
+        )
+      }}
+    </div>
+    <div class="text-bold text-center text-white">
+      {{ props.date.getDate() }}
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { PropType } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const i18n = useI18n()
 
 const props = defineProps({
   date: {
