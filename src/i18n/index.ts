c5832f228358e24c77b7e41b762a2aeca5d6dded
Deni Baskovsky <qertis@users.noreply.github.com>
2024-11-05 16:13:51 +0300

    Locale refactoring

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,20 @@
-import ru from './ru'
-import en from './en'
+import ru from './ru-RU'
+import en from './en-US'
 
 // https://schedule-x.dev/docs/calendar/supported-languages
 export enum Locale {
-  'ru-RU',
-  'en-GB',
-  'en-US',
+  'ru-RU' = 'ru-RU',
+  'en-GB' = 'en-US',
+  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
+  'en-US' = 'en-US',
 }
 
 export default {
   [Locale['ru-RU']]: ru,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   [Locale['en-GB']]: en,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   [Locale['en-US']]: en,
 }
