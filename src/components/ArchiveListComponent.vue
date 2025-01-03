948ad734746c5ef3c3a122af350b7f852ef39e4e
Deni Baskovsky <qertis@users.noreply.github.com>
2025-01-03 18:13:21 +0300

    TaskFull Component

--- src/components/ArchiveListComponent.vue
+++ src/components/ArchiveListComponent.vue
@@ -3,125 +3,15 @@
     <template
       #default="{ item, index }: { item: FormatContract; index: number }"
     >
-      <QCard
+      <TaskFull
         :key="index"
-        flat
-        square
-        bordered
+        :item="item"
         :class="{
           'q-mt-md': index > 0,
         }"
-      >
-        <div class="row">
-          <div
-            class="column q-pl-md q-pb-sm"
-            style="max-width: calc(100% - 45px)"
-          >
-            <p
-              class="full-width q-pt-md text-subtitle1 text-uppercase text-weight-bold no-margin"
-              :class="{
-                'q-pb-md': !item.instrument.description,
-                'q-pb-none':
-                  item.instrument.description && !$q.platform.is.desktop,
-                'ellipsis': $q.platform.is.desktop,
-              }"
-            >
-              <template v-if="isVerified(item, publicKey)">
-                <QIcon name="verified" />
-              </template>
-              <template v-else>
-                <QIcon name="error" color="warning" />
-              </template>
-              {{ item.instrument.name }}
-              <QTooltip>{{ item.instrument.name }}</QTooltip>
-            </p>
-            <div
-              v-if="item.instrument.description"
-              class="full-width text-caption q-pb-md text-grey no-margin"
-              v-html="parse(item.instrument.description)"
-            ></div>
-          </div>
-          <QSpace />
-          <QBtn
-            size="md"
-            class="q-ml-auto q-mr-auto q-mt-none q-mb-none"
-            round
-            square
-            flat
-            icon="more_vert"
-          >
-            <QMenu transition-show="jump-down" transition-duration="200">
-              <QList bordered separator padding :dense="$q.platform.is.desktop">
-                <QItem v-close-popup clickable @click="emit('onEdit', item)">
-                  <QItemSection side>
-                    <QItemLabel
-                      v-if="isLoggedIn && item.sameAs"
-                      overline
-                      caption
-                    >
-                      {{ $t('archiveList.pod') }}
-                    </QItemLabel>
-                    <QItemLabel class="text-uppercase">
-                      {{ $t('archiveList.edit') }}
-                    </QItemLabel>
-                  </QItemSection>
-                </QItem>
-                <QItem v-close-popup clickable @click="emit('onRemove', item)">
-                  <QItemSection side>
-                    <QItemLabel
-                      v-if="isLoggedIn && item.sameAs"
-                      overline
-                      caption
-                    >
-                      {{ $t('archiveList.pod') }}
-                    </QItemLabel>
-                    <QItemLabel class="text-negative text-uppercase">
-                      {{ $t('archiveList.remove') }}
-                    </QItemLabel>
-                  </QItemSection>
-                </QItem>
-              </QList>
-            </QMenu>
-          </QBtn>
-        </div>
-        <template v-if="item.object.length">
-          <QSeparator />
-          <!--  FIXME: здесь вместо карусели будет только PDF -->
-          <ContractCarouselComponent :model="item" />
-        </template>
-        <QSeparator />
-        <QCardSection>
-          <QBtn
-            fab-mini
-            color="white"
-            text-color="accent"
-            icon="send"
-            class="absolute"
-            style="top: 0; left: 18px; transform: translateY(-50%)"
-            @click="onSheet(item)"
-          >
-            <QTooltip>
-              {{ $t('archiveList.shareFile') }}
-            </QTooltip>
-          </QBtn>
-          <div class="flex content-center text-overline no-margin q-pt-sm">
-            <QIcon
-              style="align-self: center"
-              class="q-pr-xs"
-              :name="itemScheduled(item) ? 'history_toggle_off' : 'schedule'"
-              :color="itemScheduled(item) ? 'negative' : 'orange-9'"
-            />
-            <span
-              :class="itemScheduled(item) ? 'text-negative' : 'text-orange-9'"
-            >
-              {{ prettyDate(item) }}
-            </span>
-          </div>
-          <div class="row items-center">
-            <ContractStory :item="item" />
-          </div>
-        </QCardSection>
-      </QCard>
+        :description="item.instrument.description"
+        :title="item.instrument.name"
+      />
     </template>
     <template v-if="paginationCount > 0" #after>
       <QSeparator class="q-ma-md" />
@@ -144,46 +34,11 @@
 </template>
 <script lang="ts" setup>
 import { PropType, ref, toRef, watch } from 'vue'
-import { useI18n } from 'vue-i18n'
-import {
-  useQuasar,
-  QBtn,
-  QIcon,
-  QItemLabel,
-  QSeparator,
-  QSpace,
-  QItemSection,
-  QList,
-  QTooltip,
-  QCardSection,
-  QCard,
-  QItem,
-  QMenu,
-  QPagination,
-  QVirtualScroll,
-  copyToClipboard,
-} from 'quasar'
-import { storeToRefs } from 'pinia'
-import useAuthStore from 'stores/auth'
-import useWalletStore from 'stores/wallet'
-import useLangStore from 'stores/lang'
-import ContractCarouselComponent from 'components/ContractCarouselComponent.vue'
-import ContractStory from 'components/ContractStory.vue'
+import { useQuasar, QSeparator, QPagination, QVirtualScroll } from 'quasar'
+import TaskFull from 'components/TaskFull.vue'
 import { FormatContract } from '../types/models'
-import { isDateNotOk } from '../helpers/dateHelper'
-import { parse } from '../helpers/markdownHelper'
-import { readFilesPromise, fileShare, canShare } from '../helpers/fileHelper'
-import { createCal, googleCalendarUrl } from '../helpers/calendarHelper'
-import { mailUrl } from '../helpers/mailHelper'
-import { isVerified } from '../helpers/contractHelper'
-import { open } from '../helpers/urlHelper'
-import { openMap } from '../services/geoService'
-import { TELEGRAM_BOT_NAME } from '../services/telegram'
 
 const $q = useQuasar()
-const i18n = useI18n()
-const langStore = useLangStore()
-const $t = i18n.t
 
 const props = defineProps({
   paginationCount: {
@@ -203,14 +58,10 @@ const props = defineProps({
     required: true,
   },
 })
-const emit = defineEmits(['onPaginate', 'onRemove', 'onEdit'])
-const authStore = useAuthStore()
-const walletStore = useWalletStore()
+const emit = defineEmits(['onPaginate'])
 
 const contracts = toRef(props, 'contracts', [])
 const page = ref(props.page)
-const { publicKey } = storeToRefs(walletStore)
-const { isLoggedIn } = storeToRefs(authStore)
 
 watch(
   () => props.page,
@@ -219,228 +70,7 @@ watch(
   },
 )
 
-function itemScheduled(item: FormatContract) {
-  return item.endTime !== null && item.endTime < new Date()
-}
-
 function getItems(from: number, size: number): FormatContract[] {
   return contracts.value.slice(from, size)
-}
-
-function prettyDate(item: FormatContract) {
-  const formatterDate = new Intl.DateTimeFormat(langStore.language, {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  })
-  if (
-    !isDateNotOk(item.startTime) &&
-    (item.endTime === null || item.endTime === undefined)
-  ) {
-    return formatterDate.format(item.startTime)
-  }
-  if (isDateNotOk(item.startTime) || isDateNotOk(item.endTime)) {
-    return ''
-  }
-  return (
-    formatterDate.format(item.startTime) +
-    ' — ' +
-    formatterDate.format(item.endTime)
-  )
-}
-
-async function shareURL(url: string) {
-  try {
-    await copyToClipboard(url)
-    $q.notify({
-      type: 'positive',
-      message: $t('components.archiveList.sheet.link.success'),
-    })
-  } catch (error) {
-    console.error(error)
-    $q.notify({
-      type: 'negative',
-      message: $t('components.archiveList.sheet.link.fail'),
-    })
-  }
-}
-
-enum Action {
-  LINK = 'link',
-  SHARE = 'share',
-  CALENDAR = 'calendar',
-  GOOGLE_CALENDAR = 'google-calendar',
-  MAIL = 'mail',
-  TELEPHONE = 'telephone',
-  LAW = 'law',
-  MAP = 'map',
-}
-
-type SheetAction = {
-  label?: string
-  icon?: string
-  color?: string
-  id?: Action
-}
-
-function onSheet(item: FormatContract) {
-  let actions: SheetAction[] = []
-  const group1 = [] // Share local
-  if (canShare) {
-    group1.push({
-      label: $t('components.archiveList.sheet.share.label'),
-      icon: $q.platform.is.android ? 'share' : 'ios_share',
-      color: 'info',
-      id: Action.SHARE,
-    })
-  }
-  group1.push({
-    label: $t('components.archiveList.sheet.event.native.label'),
-    icon: 'event',
-    color: 'info',
-    id: Action.CALENDAR,
-  })
-  if (group1.length) {
-    actions = actions.concat(group1)
-    actions.push({})
-  }
-  const group2 = [] // Publish Group
-  if (item.sameAs) {
-    group2.push({
-      label: $t('components.archiveList.sheet.link.label'),
-      icon: 'link',
-      color: 'primary',
-      id: Action.LINK,
-    })
-  }
-  if ($q.platform.is.android) {
-    group2.push({
-      label: $t('components.archiveList.sheet.event.google.label'),
-      icon: 'event',
-      color: 'secondary',
-      id: Action.GOOGLE_CALENDAR,
-    })
-  }
-  if (group2.length) {
-    actions = actions.concat(group2)
-    actions.push({})
-  }
-  const group3 = [] // Message Group
-  if (item.participant.email || item.participant.telephone) {
-    if (item.participant.email) {
-      group3.push({
-        label: $t('components.archiveList.sheet.mail.label'),
-        icon: 'contact_mail',
-        color: 'secondary',
-        id: Action.MAIL,
-      })
-    }
-    if (item.participant.telephone) {
-      group3.push({
-        label: $t('components.archiveList.sheet.telephone.label'),
-        icon: 'call',
-        color: 'secondary',
-        id: Action.TELEPHONE,
-      })
-    }
-    if (isVerified(item, publicKey.value)) {
-      group3.push({
-        label: $t('components.archiveList.sheet.law.label'),
-        icon: 'gavel',
-        color: 'secondary',
-        id: Action.LAW,
-      })
-    }
-  }
-  if (item.location && Object.keys(item.location).length > 0) {
-    group3.push({
-      label: $t('components.archiveList.sheet.map.label'),
-      icon: 'map',
-      color: 'secondary',
-      id: Action.MAP,
-    })
-  }
-  if (group3.length) {
-    actions = actions.concat(group3)
-  }
-  const icalId = $t('organization.prodid')
-
-  $q.bottomSheet({
-    title: $t('components.archiveList.sheet.title'),
-    grid: !$q.platform.is.mobile,
-    class: $q.platform.is.desktop ? 'text-center' : '',
-    actions: actions,
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  }).onOk((action: { id: SheetAction }) => {
-    switch (action.id) {
-      case Action.SHARE: {
-        const icalFile = createCal(icalId, item)
-        return shareFile(item.instrument.name, icalFile)
-      }
-      case Action.LINK: {
-        const shareLink = window.location.origin + '/sign?from=' + item.sameAs
-        return shareURL(shareLink)
-      }
-      case Action.GOOGLE_CALENDAR: {
-        const url = googleCalendarUrl(item).toString()
-        return open(url)
-      }
-      case Action.CALENDAR: {
-        const file = createCal(icalId, item)
-        return saveIcal(file)
-      }
-      case Action.MAIL: {
-        return open(mailUrl(item))
-      }
-      case Action.TELEPHONE: {
-        return open(item.participant.telephone)
-      }
-      case Action.LAW: {
-        return sendToCourt()
-      }
-      case Action.MAP: {
-        return openMap(item.location)
-      }
-      default: {
-        console.warn('Unknown id')
-        break
-      }
-    }
-  })
-}
-
-function sendToCourt() {
-  console.warn('This functionality is under development.')
-  const myAction = 'start'
-  open(`https://t.me/${TELEGRAM_BOT_NAME}?start=${myAction}`)
-}
-
-async function saveIcal(file: File) {
-  try {
-    const [dataURI] = await readFilesPromise([file])
-    const link = document.createElement('a')
-    link.href = dataURI
-    link.setAttribute('download', file.name)
-    link.setAttribute('target', '_blank')
-    link.click()
-  } catch (error) {
-    console.error(error)
-    $q.notify({
-      type: 'negative',
-      message: $t('components.archiveList.sheet.share.fail'),
-    })
-  }
-}
-
-async function shareFile(title: string, file: File) {
-  try {
-    await fileShare(file, title)
-  } catch (error) {
-    console.error('Sharing failed', error)
-    $q.notify({
-      type: 'negative',
-      message: $t('components.archiveList.sheet.share.fail'),
-    })
-  }
 }
 </script>
---
+++ src/components/TaskFull.vue
@@ -0,0 +1,381 @@
+<template>
+  <QCard flat square bordered>
+    <div class="row">
+      <div class="column q-pl-md q-pb-sm" style="max-width: calc(100% - 45px)">
+        <p
+          class="full-width q-pt-md text-subtitle1 text-uppercase text-weight-bold no-margin"
+          :class="{
+            'q-pb-md': !description,
+            'q-pb-none': description && !$q.platform.is.desktop,
+            'ellipsis': $q.platform.is.desktop,
+          }"
+        >
+          <template v-if="isVerified(item, publicKey)">
+            <QIcon name="verified" />
+          </template>
+          <template v-else>
+            <QIcon name="error" color="warning" />
+          </template>
+          {{ title }}
+          <QTooltip>{{ title }}</QTooltip>
+        </p>
+        <div
+          v-if="item.instrument.description"
+          class="full-width text-caption q-pb-md text-grey no-margin"
+          v-html="parse(description)"
+        ></div>
+      </div>
+      <QSpace />
+      <QBtn
+        size="md"
+        class="q-ml-auto q-mr-auto q-mt-none q-mb-none"
+        round
+        square
+        flat
+        icon="more_vert"
+      >
+        <QMenu transition-show="jump-down" transition-duration="200">
+          <QList bordered separator padding :dense="$q.platform.is.desktop">
+            <QItem v-close-popup clickable @click="emit('onEdit', item)">
+              <QItemSection side>
+                <QItemLabel v-if="isLoggedIn && item.sameAs" overline caption>
+                  {{ $t('archiveList.pod') }}
+                </QItemLabel>
+                <QItemLabel class="text-uppercase">
+                  {{ $t('archiveList.edit') }}
+                </QItemLabel>
+              </QItemSection>
+            </QItem>
+            <QItem v-close-popup clickable @click="emit('onRemove', item)">
+              <QItemSection side>
+                <QItemLabel v-if="isLoggedIn && item.sameAs" overline caption>
+                  {{ $t('archiveList.pod') }}
+                </QItemLabel>
+                <QItemLabel class="text-negative text-uppercase">
+                  {{ $t('archiveList.remove') }}
+                </QItemLabel>
+              </QItemSection>
+            </QItem>
+          </QList>
+        </QMenu>
+      </QBtn>
+    </div>
+    <template v-if="item.object.length">
+      <QSeparator />
+      <ContractCarouselComponent :model="item" />
+    </template>
+    <QSeparator />
+    <QCardSection>
+      <QBtn
+        fab-mini
+        color="white"
+        text-color="accent"
+        icon="send"
+        class="absolute"
+        style="top: 0; left: 18px; transform: translateY(-50%)"
+        @click="onSheet(item)"
+      >
+        <QTooltip>
+          {{ $t('archiveList.shareFile') }}
+        </QTooltip>
+      </QBtn>
+      <div class="flex content-center text-overline no-margin q-pt-sm">
+        <QIcon
+          style="align-self: center"
+          class="q-pr-xs"
+          :name="itemScheduled(item) ? 'history_toggle_off' : 'schedule'"
+          :color="itemScheduled(item) ? 'negative' : 'orange-9'"
+        />
+        <span :class="itemScheduled(item) ? 'text-negative' : 'text-orange-9'">
+          {{ prettyDate(item) }}
+        </span>
+      </div>
+      <div class="row items-center">
+        <ContractStory :item="item" />
+      </div>
+    </QCardSection>
+  </QCard>
+</template>
+<script lang="ts" setup>
+import { PropType } from 'vue'
+import {
+  useQuasar,
+  QBtn,
+  QIcon,
+  QItemLabel,
+  QSeparator,
+  QSpace,
+  QItemSection,
+  QList,
+  QTooltip,
+  QCardSection,
+  QCard,
+  QItem,
+  QMenu,
+  copyToClipboard,
+} from 'quasar'
+import { useI18n } from 'vue-i18n'
+import { storeToRefs } from 'pinia'
+import useAuthStore from 'stores/auth'
+import useWalletStore from 'stores/wallet'
+import ContractCarouselComponent from 'components/ContractCarouselComponent.vue'
+import ContractStory from 'components/ContractStory.vue'
+import useLangStore from 'stores/lang'
+import { parse } from '../helpers/markdownHelper'
+import { isDateNotOk } from '../helpers/dateHelper'
+import { readFilesPromise, fileShare, canShare } from '../helpers/fileHelper'
+import { createCal, googleCalendarUrl } from '../helpers/calendarHelper'
+import { mailUrl } from '../helpers/mailHelper'
+import { isVerified } from '../helpers/contractHelper'
+import { open } from '../helpers/urlHelper'
+import { openMap } from '../services/geoService'
+import { TELEGRAM_BOT_NAME } from '../services/telegram'
+import { FormatContract } from '../types/models'
+
+const emit = defineEmits(['onRemove', 'onEdit'])
+const props = defineProps({
+  item: {
+    type: Object as PropType<FormatContract>,
+    required: true,
+  },
+  title: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  description: {
+    type: String as PropType<string>,
+    default: '',
+  },
+})
+
+const $q = useQuasar()
+const i18n = useI18n()
+const walletStore = useWalletStore()
+const authStore = useAuthStore()
+const langStore = useLangStore()
+const $t = i18n.t
+
+const { isLoggedIn } = storeToRefs(authStore)
+const { publicKey } = storeToRefs(walletStore)
+
+function itemScheduled(item: FormatContract) {
+  return item.endTime !== null && item.endTime < new Date()
+}
+
+function prettyDate(item: FormatContract) {
+  const formatterDate = new Intl.DateTimeFormat(langStore.language, {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+  if (
+    !isDateNotOk(item.startTime) &&
+    (item.endTime === null || item.endTime === undefined)
+  ) {
+    return formatterDate.format(item.startTime)
+  }
+  if (isDateNotOk(item.startTime) || isDateNotOk(item.endTime)) {
+    return ''
+  }
+  return (
+    formatterDate.format(item.startTime) +
+    ' — ' +
+    formatterDate.format(item.endTime)
+  )
+}
+
+function onSheet(item: FormatContract) {
+  let actions: SheetAction[] = []
+  const group1 = [] // Share local
+  if (canShare) {
+    group1.push({
+      label: $t('components.archiveList.sheet.share.label'),
+      icon: $q.platform.is.android ? 'share' : 'ios_share',
+      color: 'info',
+      id: Action.SHARE,
+    })
+  }
+  group1.push({
+    label: $t('components.archiveList.sheet.event.native.label'),
+    icon: 'event',
+    color: 'info',
+    id: Action.CALENDAR,
+  })
+  if (group1.length) {
+    actions = actions.concat(group1)
+    actions.push({})
+  }
+  const group2 = [] // Publish Group
+  if (item.sameAs) {
+    group2.push({
+      label: $t('components.archiveList.sheet.link.label'),
+      icon: 'link',
+      color: 'primary',
+      id: Action.LINK,
+    })
+  }
+  if ($q.platform.is.android) {
+    group2.push({
+      label: $t('components.archiveList.sheet.event.google.label'),
+      icon: 'event',
+      color: 'secondary',
+      id: Action.GOOGLE_CALENDAR,
+    })
+  }
+  if (group2.length) {
+    actions = actions.concat(group2)
+    actions.push({})
+  }
+  const group3 = [] // Message Group
+  if (item.participant.email || item.participant.telephone) {
+    if (item.participant.email) {
+      group3.push({
+        label: $t('components.archiveList.sheet.mail.label'),
+        icon: 'contact_mail',
+        color: 'secondary',
+        id: Action.MAIL,
+      })
+    }
+    if (item.participant.telephone) {
+      group3.push({
+        label: $t('components.archiveList.sheet.telephone.label'),
+        icon: 'call',
+        color: 'secondary',
+        id: Action.TELEPHONE,
+      })
+    }
+    if (isVerified(item, publicKey.value)) {
+      group3.push({
+        label: $t('components.archiveList.sheet.law.label'),
+        icon: 'gavel',
+        color: 'secondary',
+        id: Action.LAW,
+      })
+    }
+  }
+  if (item.location && Object.keys(item.location).length > 0) {
+    group3.push({
+      label: $t('components.archiveList.sheet.map.label'),
+      icon: 'map',
+      color: 'secondary',
+      id: Action.MAP,
+    })
+  }
+  if (group3.length) {
+    actions = actions.concat(group3)
+  }
+  const icalId = $t('organization.prodid')
+
+  $q.bottomSheet({
+    title: $t('components.archiveList.sheet.title'),
+    grid: !$q.platform.is.mobile,
+    class: $q.platform.is.desktop ? 'text-center' : '',
+    actions: actions,
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  }).onOk((action: { id: SheetAction }) => {
+    switch (action.id) {
+      case Action.SHARE: {
+        const icalFile = createCal(icalId, item)
+        return shareFile(item.instrument.name, icalFile)
+      }
+      case Action.LINK: {
+        const shareLink = window.location.origin + '/sign?from=' + item.sameAs
+        return shareURL(shareLink)
+      }
+      case Action.GOOGLE_CALENDAR: {
+        const url = googleCalendarUrl(item).toString()
+        return open(url)
+      }
+      case Action.CALENDAR: {
+        const file = createCal(icalId, item)
+        return saveIcal(file)
+      }
+      case Action.MAIL: {
+        return open(mailUrl(item))
+      }
+      case Action.TELEPHONE: {
+        return open(item.participant.telephone)
+      }
+      case Action.LAW: {
+        return sendToCourt()
+      }
+      case Action.MAP: {
+        return openMap(item.location)
+      }
+      default: {
+        console.warn('Unknown id')
+        break
+      }
+    }
+  })
+}
+
+enum Action {
+  LINK = 'link',
+  SHARE = 'share',
+  CALENDAR = 'calendar',
+  GOOGLE_CALENDAR = 'google-calendar',
+  MAIL = 'mail',
+  TELEPHONE = 'telephone',
+  LAW = 'law',
+  MAP = 'map',
+}
+
+type SheetAction = {
+  label?: string
+  icon?: string
+  color?: string
+  id?: Action
+}
+
+function sendToCourt() {
+  console.warn('This functionality is under development.')
+  const myAction = 'start'
+  open(`https://t.me/${TELEGRAM_BOT_NAME}?start=${myAction}`)
+}
+
+async function saveIcal(file: File) {
+  try {
+    const [dataURI] = await readFilesPromise([file])
+    const link = document.createElement('a')
+    link.href = dataURI
+    link.setAttribute('download', file.name)
+    link.setAttribute('target', '_blank')
+    link.click()
+  } catch (error) {
+    console.error(error)
+    $q.notify({
+      type: 'negative',
+      message: $t('components.archiveList.sheet.share.fail'),
+    })
+  }
+}
+
+async function shareFile(title: string, file: File) {
+  try {
+    await fileShare(file, title)
+  } catch (error) {
+    console.error('Sharing failed', error)
+    $q.notify({
+      type: 'negative',
+      message: $t('components.archiveList.sheet.share.fail'),
+    })
+  }
+}
+
+async function shareURL(url: string) {
+  try {
+    await copyToClipboard(url)
+    $q.notify({
+      type: 'positive',
+      message: $t('components.archiveList.sheet.link.success'),
+    })
+  } catch (error) {
+    console.error(error)
+    $q.notify({
+      type: 'negative',
+      message: $t('components.archiveList.sheet.link.fail'),
+    })
+  }
+}
+</script>
